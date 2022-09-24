2e8eb2a9636d8448b94c774febed9e753644a7a3
MD. Liton Hossain <litoneng3@gmail.com>
2022-09-25 03:26:50 +0600

    last

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+import axios from "axios";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
+import styles from "../styles/Home.module.css";
+
+export default function Home({pizzaList}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured/>
+      <PizzaList pizzaList={pizzaList}/>
+    </div>
+  );
+}
+
+
+export const getServerSideProps = async (ctx) => {
+  
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      pizzaList: res.data
+    },
+  };
+};
